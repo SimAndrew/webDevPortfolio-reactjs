75af662bba8413cdd70d0ef1bffefa6b7ccfb1ab
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2026-01-03 20:42:25 +0200

    feat: implement Navbar component and add data structure for links, skills, and projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Navbar from './components/Navbar.jsx';
+
 function App() {
-	return <h2 class="text-3xl font-bold underline">Web Dev Portfolio</h2>;
+	return (
+		<>
+			<Navbar />
+		</>
+	);
 }
 
 export default App;
