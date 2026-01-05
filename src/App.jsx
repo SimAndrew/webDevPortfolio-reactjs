ae5894295eccb72f6b830e7aaf0eecc186c042b0
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2026-01-05 20:55:48 +0200

    feat: add Skills section with dynamic skill cards and update links

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
+import Skills from './components/Skills.jsx';
 
 function App() {
 	return (
 		<>
 			<Navbar />
 			<Hero />
+			<Skills />
 		</>
 	);
 }
--- src/data.jsx
+++ src/data.jsx
@@ -4,8 +4,8 @@ import { RiNextjsLine } from 'react-icons/ri';
 
 export const links = [
 	{ id: nanoid(), href: '#home', text: 'home' },
-	{ id: nanoid(), href: '#about', text: 'about' },
 	{ id: nanoid(), href: '#skills', text: 'skills' },
+	{ id: nanoid(), href: '#about', text: 'about' },
 	{ id: nanoid(), href: '#projects', text: 'projects' },
 ];
 
